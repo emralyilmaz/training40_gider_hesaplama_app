331b5c2e8f1238ce2b9aab58ca40ecb045f0ed65
Emral Yılmaz <emralyilmaz@Emrals-MacBook-Air.local>
2020-08-26 20:18:30 +0300

    Bottom  Navbar Oluşturma

---
+++ lib/screens/gider_listesi.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class GiderListesi extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/screens/kategori_gider_listesi.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class KategoriGiderListesi extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
