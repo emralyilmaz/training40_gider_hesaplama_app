8516cfe3580ca0c6dc604b20f3c56ef5da408e76
Emral Yılmaz <emralyilmaz@Emrals-MacBook-Air.local>
2020-08-27 12:07:07 +0300

    Kategori Listesi Sayfasını Oluşturma

--- lib/screens/kategori_listesi.dart
+++ lib/screens/kategori_listesi.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:training40_gider_hesaplama_app/models/kategori.dart';
+import 'package:training40_gider_hesaplama_app/tools/database_helper.dart';
 
 class KategoriListesi extends StatefulWidget {
   @override
@@ -6,12 +8,49 @@ class KategoriListesi extends StatefulWidget {
 }
 
 class _KategoriListesiState extends State<KategoriListesi> {
+  List<Kategori> kategoriler;
+  DatabaseHelper databaseHelper;
+
+  @override
+  void initState() {
+    databaseHelper = DatabaseHelper();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
+    if (kategoriler == null) {
+      kategoriler = List<Kategori>();
+      // kategori listesini güncelleme işlemi yapılacak
+    }
+
     return Scaffold(
       appBar: AppBar(
-        title: Text("Kategoriler"),
+        title: Text("Kategoriler",
+            style: TextStyle(color: Color.fromRGBO(3, 54, 73, 1))),
+        actions: [
+          FlatButton(
+              onPressed: () {}, // kategori ekle
+              child: Icon(Icons.add))
+        ],
+      ),
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(kategoriler[index].kategoriAd),
+            trailing: Icon(Icons.delete_forever),
+          );
+        },
+        itemCount: kategoriler.length,
       ),
     );
+  }
+
+  void kategoriListesiniGuncelle() {
+    databaseHelper.kategoriListesiniGetir().then((katList) {
+      setState(() {
+        kategoriler = katList;
+      });
+    });
   }
 }
